57eb4d94a706551a88a54a16f957494ba2c68db1
Adriaan van Rossum <git@adriaan.io>
2015-02-26 17:04:16 +0100

    Through error on booting when nginx is not found

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,4 +8,9 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
+if [ ! -f $HOME/vendor/nginx/sbin/nginx ]; then
+  echo "Nginx not found in $HOME/vendor/nginx/sbin/nginx while booting"
+  exit 1
+fi
+
 exec $HOME/vendor/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
